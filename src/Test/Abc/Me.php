4979ffb0f4e7b4a98be3d122291f2597e421d3e5
mbq <329673018@qq.com>
2020-06-28 16:35:33 +0800

    test2

---
+++ src/Test/Abc/Me.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mbq
+ * Date: 2020/6/28
+ * Time: 16:32
+ */
+namespace Abc;
+class Me{
+    public static function haha()
+    {
+        return 'haha hello';
+    }
+}
